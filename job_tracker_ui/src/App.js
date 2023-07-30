30ef7d909954cef4c75c920846ba712f845f9330
jiajie wang <55546624+jwang483@users.noreply.github.com>
2023-07-29 20:52:36 -0400

    Add files via upload

--- job_tracker_ui/src/App.js
+++ job_tracker_ui/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import { Routes } from 'react-router';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
@@ -9,6 +9,8 @@ import JobPage from './pages/JobPage';
 import SkillsPage from './pages/SkillsPage';
 import ContactPage from './pages/ContactPage';
 import EditSkillsPage from './pages/EditSkillsPage';
+import EditJobPage from './pages/EditJobPage';
+import EditContactPage from './pages/EditContactPage';
 
 export const App = () => {
     const [targetSkill, setTargetSkill] = useState();
@@ -20,9 +22,11 @@ export const App = () => {
               <Route path="/register" element={<RegisterPage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/jobs" element={<JobPage />} />
+              <Route path="/jobs/edit/:id" element={<EditJobPage  />} />
               <Route path="/skills" element={<SkillsPage setTargetSkill={setTargetSkill} />} />
               <Route path="/edit-skill" element={<EditSkillsPage targetSkill={targetSkill} />} />
               <Route path="/contacts" element={<ContactPage />} />
+              <Route path="/contacts/edit/:id" element={<EditContactPage />} />
           </Routes>
       </BrowserRouter>
      </div>
